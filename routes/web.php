0ad2eae2adffaa32b407103aa1dfda3ffa18f48b
gempurbayu <aji123zz@gmail.com>
2018-01-22 10:03:49 +0700

    show data & template

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'UserController@index')->name('home');
+Route::resource('users','UserController');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
