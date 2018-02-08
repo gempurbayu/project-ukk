8a0dbc33f7138a32fb2d97e744de4e1f06821ed3
gempurbayu <aji123zz@gmail.com>
2018-02-09 00:06:10 +0700

    bikin middleware

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home',function(){
+	return redirect('/admin');
+});
 
 Auth::routes();
 
+Route::middleware('admin')->group(function(){
 Route::get('/admin', 'UserController@index');
 Route::get('/customers', 'CustomerController@index');
 Route::get('/customers/create', 'CustomerController@create');
 Route::get('/customers/edit/{id}', 'CustomerController@edit');
 Route::resource('users','UserController');
 Route::resource('customers','CustomerController');
+
+
+});
 
